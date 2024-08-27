ee253dd218140d632b9a87b92105363b6d5961f1
TBLogan <104415634+TBLogan@users.noreply.github.com>
2024-08-27 16:27:37 +0200

    Various bug fixes
    Bugs preventing app from running, particularly preferences.xml. Implemented dependency in build.gradle(Module:App)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,9 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:23.0.0")
     implementation("com.google.firebase:firebase-messaging:24.0.1")
     implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("androidx.preference:preference:1.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
