93ad3dc30bff7b6b1aa8e01de7ce9e6d9962d02e
TBLogan <104415634+TBLogan@users.noreply.github.com>
2024-09-15 11:23:58 +0200

    Added Home Button to About screen
    - Set navigation to home screen from about screen

--- app/src/main/java/com/example/heal_application/AboutActivity.kt
+++ app/src/main/java/com/example/heal_application/AboutActivity.kt
@@ -1,11 +1,25 @@
 package com.example.heal_application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
+private lateinit var homeButton: Button
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        homeButton = findViewById(R.id.homeButton)
+
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()  // Close EventsActivity
+        }
     }
+
+
 }
