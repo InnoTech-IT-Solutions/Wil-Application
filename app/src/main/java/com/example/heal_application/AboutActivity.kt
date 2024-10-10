7bf71adee261ff5330eb62e0fa86933ae02b9798
TBLogan <104415634+TBLogan@users.noreply.github.com>
2024-10-10 13:11:26 +0200

    OSM Map added
    - Added OpenStreetMap webview function to About

--- app/src/main/java/com/example/heal_application/AboutActivity.kt
+++ app/src/main/java/com/example/heal_application/AboutActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 private lateinit var homeButton: Button
 class AboutActivity : AppCompatActivity() {
@@ -11,8 +13,21 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
+        val webView: WebView = findViewById(R.id.mapView)
         homeButton = findViewById(R.id.homeButton)
 
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // Load the OSM map with the desired coordinates
+        val latitude = -29.8291774  // Replace with your NGO's latitude
+        val longitude = 30.9982982 // Replace with your NGO's longitude
+        val zoomLevel = 20  // Adjust as needed for more/less zoom
+
+
+        val mapUrl = "https://www.openstreetmap.org/?mlat=$latitude&mlon=$longitude#map=$zoomLevel/$latitude/$longitude"
+        webView.loadUrl(mapUrl)
 
         homeButton.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
